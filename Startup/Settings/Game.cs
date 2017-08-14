1a1cf4cb93fcbd126683263442077b01319ee8f4
Dimitar Nikolov <zeitgeistdimi@gmail.com>
2017-08-14 22:59:19 +0300

    Adding property for PointEventGoodScore

--- Startup/Settings/Game.cs
+++ Startup/Settings/Game.cs
@@ -7,6 +7,7 @@ namespace Startup.Settings
         public const int EventsCount = 10; // Count of Point boxes on the map [5 - 50]
         private static int gameSpeed = 30;
         private static int pointDeductor = 1; // How much points a point box losses every game tick [ 5-99]
+        private static int pointEventGoodScore = 50; // Above that points a box is colored as good [ 5-99]
 
         public static int GameSpeed
         {
@@ -35,12 +36,27 @@ namespace Startup.Settings
                 }
                 else
                 {
-                    throw new ArgumentException("Value of box`s point cannot be lower than 0");
+                    throw new ArgumentException("Value of box points cannot be lower than 0");
                 }
             }
         }
 
-        public static int PointEventGoodScore = 50; // Above that points a box is colored as good [ 5-99]
+        public static int PointEventGoodScore
+        {
+            get { return pointEventGoodScore; }
+            set
+            {
+                if (value > 0)
+                {
+                    pointEventGoodScore = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Value of box points cannot be lower than 0");
+                }
+            }
+        }
+
         public static int PointEventBestScore = 70;// Above that points a box is colored as best [ 5-99]
         public static int HostileAddingScore = 500; // after how mutch points generated a new hostile is added
         public static int LoseLifePenalty = -100;
