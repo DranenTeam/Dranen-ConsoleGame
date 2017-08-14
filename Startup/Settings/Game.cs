164149f70db80eed87505eddd57f2a7533c6eb31
Dimitar Nikolov <zeitgeistdimi@gmail.com>
2017-08-14 22:50:25 +0300

    Adding property for PointDeductor

--- Startup/Settings/Game.cs
+++ Startup/Settings/Game.cs
@@ -4,7 +4,9 @@ namespace Startup.Settings
 {
     public static class Game
     {
+        public const int EventsCount = 10; // Count of Point boxes on the map [5 - 50]
         private static int gameSpeed = 30;
+        private static int pointDeductor = 1; // How much points a point box losses every game tick [ 5-99]
 
         public static int GameSpeed
         {
@@ -22,9 +24,22 @@ namespace Startup.Settings
             }
         }
 
-        public const int EventsCount = 10; // Count of Point boxes on the map [5 - 50]
-        public static int PointDeathThreshold = 5; // Below what amount of points a point box disappears [5 - 50]
-        public static int PointDeductor = 1; // How much points a point box losses every game tick [ 5-99]
+        public static int PointDeductor
+        {
+            get { return pointDeductor; }
+            set
+            {
+                if (value > 0)
+                {
+                    pointDeductor = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Value of box`s point cannot be lower than 0");
+                }
+            }
+        }
+
         public static int PointEventGoodScore = 50; // Above that points a box is colored as good [ 5-99]
         public static int PointEventBestScore = 70;// Above that points a box is colored as best [ 5-99]
         public static int HostileAddingScore = 500; // after how mutch points generated a new hostile is added
