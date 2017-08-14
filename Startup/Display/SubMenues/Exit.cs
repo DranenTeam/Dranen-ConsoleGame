1d325653619c5b2727f306a801afec93c6cb8e5f
Theodor <tiapko@abv.bg>
2017-08-14 22:00:40 +0300

    Menu refactored and reorganised.

---
+++ Startup/Display/SubMenues/Exit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Startup.Display.SubMenues
+{
+    public class Exit
+    {
+        public Exit()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Startup/Display/SubMenues/Greeting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Startup.Display.SubMenues
+{
+    public class Greeting
+    {
+        public Greeting()
+        {
+            Console.Clear();
+            Console.CursorVisible = false;
+            Display.Menu.Logo();
+            Startup.Menu.Position(16, 7);
+            Console.WriteLine($"Hello {Display.Menu.Result}!");
+            Startup.Menu.Position(10, 8);
+            Console.WriteLine($"Good luck in your quest!");
+        }
+    }
+}
